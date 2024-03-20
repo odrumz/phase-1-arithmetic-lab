dd8c1db6b28fe31db2ef32ca23596bd49ee6e18c
DerrickOngoma <Odrumz64@gmail.com>
2024-03-20 09:05:53 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 10
+let num4 = 6
+const mod = num3-num4
+
+let max=Math.max(9, 5, 1, 20, 13)
